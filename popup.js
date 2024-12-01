f115f4c2dcb0e4c3423be643c2aeb8b881d04d4e
Chagit <chagitalxndr@gmail.com>
2024-12-01 23:48:55 +0200

    Update display upon login

--- popup.js
+++ popup.js
@@ -1,21 +1,38 @@
 import { createOrUpdateSpreadsheet } from "./scripts/apis.js"
 
-document.getElementById('signInButton').addEventListener('click', () => {
-    chrome.identity.getAuthToken({ interactive: true }, (token) => {
-        if (chrome.runtime.lastError) {
-            console.log(`Error getting token: ${chrome.runtime.lastError.message}`);
-            return;
-        }
-        else {
-            // Create a new spreadsheet if it doesn't exist
-            chrome.storage.local.get(['spreadsheetId'], (result) => {
-                if (!result.spreadsheetId) {
-                    createOrUpdateSpreadsheet(token);
-                }
-                else {
 
-                }
-            })
+document.addEventListener("DOMContentLoaded", function () {
+
+    const signInButton = document.getElementById("signInButton")
+    const statusText = document.getElementById('statusText');
+
+    chrome.storage.local.get(['authToken'], function (result) {
+        if (result.authToken) {
+            signInButton.style.display = 'none';
+            statusText.textContent = 'You are signed in';
+        } else {
+            signInButton.style.display = 'block';
+            statusText.textContent = 'Please sign in';
         }
-    })
+    });
+
+    signInButton.addEventListener('click', function () {
+        chrome.identity.getAuthToken({ interactive: true }, (token) => {
+            if (chrome.runtime.lastError) {
+                console.log(`Error getting token: ${chrome.runtime.lastError.message}`);
+            }
+            else {
+                // Save the token to local storage and update the button status
+                chrome.storage.local.set({ 'authToken': token }, () => {
+                    signInButton.style.display = 'none';
+                    statusText.textContent = 'You are signed in';
+                });
+                // Create a new spreadsheet if it doesn't exist
+                chrome.storage.local.get(['spreadsheetId'], () => {
+                    createOrUpdateSpreadsheet(token);
+                })
+            }
+        });
+    });
 })
+
